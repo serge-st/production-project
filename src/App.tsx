d6ac4bb9240220e925fc3446b6e5d38bc44f3127
Serge Stecenko <sergejs.stecenko@gmail.com>
2022-09-29 18:26:45 +0300

    add react routers

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import { Counter } from "./components/Counter";
+import { Suspense } from "react";
+import { Link } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import './index.scss';
+import AboutPage from "./pages/AboutPage";
+import MainPage from "./pages/MainPage";
 
 const App = () => {
     return (
         <div className="app">
-            Counter
-            <Counter />
+            <Link to={'/'}>Home</Link>
+            <Link to={'/about'}>About</Link>
+
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path={'/'} element={<MainPage />} />
+                    <Route path={'/about'} element={<AboutPage />} />
+                </Routes>
+            </Suspense>
         </div>
     );
 };
