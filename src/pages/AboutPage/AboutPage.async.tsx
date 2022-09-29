e41814f759e0fb9e1f7c7ded5709e4c5ceaf6a3a
Serge Stecenko <sergejs.stecenko@gmail.com>
2022-09-29 19:04:31 +0300

    implement code splitting and async components

--- src/pages/AboutPage/AboutPage.async.tsx
+++ src/pages/AboutPage/AboutPage.async.tsx
@@ -1,5 +1,8 @@
 import { lazy } from 'react';
 
+// export const AboutPageAsync = lazy(() => import('./AboutPage'));
+
+// Just for Suspense testing:
 export const AboutPageAsync = lazy(() => new Promise(resolve => {
     // @ts-ignore
     setTimeout(() => resolve(import('./AboutPage')), 1500)
