39fb622822d73985b8eee570952b821b02b55baa
Serge Stecenko <sergejs.stecenko@gmail.com>
2022-09-29 19:02:10 +0300

    implement code splitting and async components

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import { Suspense } from "react";
 import { Link } from "react-router-dom";
 import { Route, Routes } from "react-router-dom";
 import './index.scss';
-import AboutPage from "./pages/AboutPage";
-import MainPage from "./pages/MainPage";
+import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
+import { MainPageAsync } from "./pages/MainPage/MainPage.async";
 
 const App = () => {
     return (
@@ -13,8 +13,8 @@ const App = () => {
 
             <Suspense fallback={<div>Loading...</div>}>
                 <Routes>
-                    <Route path={'/'} element={<MainPage />} />
-                    <Route path={'/about'} element={<AboutPage />} />
+                    <Route path={'/'} element={<MainPageAsync />} />
+                    <Route path={'/about'} element={<AboutPageAsync />} />
                 </Routes>
             </Suspense>
         </div>
