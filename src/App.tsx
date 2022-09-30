0a0eb4cd93abd8507626d3493a15e92eacca44a2
Serge Stecenko <sergejs.stecenko@gmail.com>
2022-09-30 15:00:19 +0300

    add theme toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
 import { Link } from "react-router-dom";
 import { Route, Routes } from "react-router-dom";
 import './styles/index.scss';
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
 
+export enum Theme {
+    LIGHT = 'light',
+    DARK = 'dark',
+}
+
 const App = () => {
+    const [theme, setTheme] = useState<Theme>(Theme.LIGHT);
+
+    const toggleTheme = () => {
+        setTheme(theme === Theme.DARK ? Theme.LIGHT : Theme.DARK);
+    }
+
     return (
-        <div className="app">
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>Change Theme</button>
+
             <Link to={'/'}>Home</Link>
             <Link to={'/about'}>About</Link>
 
