b5cd85d0fa44b73e0ef7abe7b00bf768cb16e3ae
Serge Stecenko <sergejs.stecenko@gmail.com>
2022-09-30 20:18:48 +0300

    create useTheme hook

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,12 @@ import './styles/index.scss';
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
 import { Theme, ThemeContext } from "./theme/ThemeContext";
+import { useTheme } from "./theme/useTheme";
 
 
 
 const App = () => {
-    const {theme, setTheme} = useContext(ThemeContext);
-
-    const toggleTheme = () => {
-        setTheme(theme === Theme.DARK ? Theme.LIGHT : Theme.DARK);
-    }
+    const {theme, toggleTheme} = useTheme();
 
     return (
         <div className={`app ${theme}`}>
