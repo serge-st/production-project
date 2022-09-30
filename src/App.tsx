8715f7726a2837616d88f2061a851a97e5b7405c
Serge Stecenko <sergejs.stecenko@gmail.com>
2022-09-30 20:01:47 +0300

    extract theme logic to theme context/provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
-import { Suspense, useState } from "react";
+import { Suspense, useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { Route, Routes } from "react-router-dom";
 import './styles/index.scss';
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+import { Theme, ThemeContext } from "./theme/ThemeContext";
 
-export enum Theme {
-    LIGHT = 'light',
-    DARK = 'dark',
-}
+
 
 const App = () => {
-    const [theme, setTheme] = useState<Theme>(Theme.LIGHT);
+    const {theme, setTheme} = useContext(ThemeContext);
 
     const toggleTheme = () => {
         setTheme(theme === Theme.DARK ? Theme.LIGHT : Theme.DARK);
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import ThemeProvider from './theme/ThemeProvider';
 
 const rootElement = document.getElementById('root');
 
 ReactDOM.render(
     <BrowserRouter>
-        <App />
+        <ThemeProvider>
+            <App />
+        </ThemeProvider>
     </BrowserRouter>,
     rootElement
 );
